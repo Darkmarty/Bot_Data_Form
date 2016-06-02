ebfbf7a7fe4a5f1f35c2496c8020b48c8ee192f1
Darkmarty <unal.batuhan@outlook.com>
2016-06-02 15:46:17 +0300

    Add files via upload

---
+++ Data_Bot/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Data_Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitle = new System.Windows.Forms.Label();
+            this.btntitle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltitle
+            // 
+            this.lbltitle.AutoSize = true;
+            this.lbltitle.Location = new System.Drawing.Point(184, 71);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(27, 13);
+            this.lbltitle.TabIndex = 0;
+            this.lbltitle.Text = "Title";
+            // 
+            // btntitle
+            // 
+            this.btntitle.Location = new System.Drawing.Point(163, 143);
+            this.btntitle.Name = "btntitle";
+            this.btntitle.Size = new System.Drawing.Size(75, 23);
+            this.btntitle.TabIndex = 1;
+            this.btntitle.Text = "Gönder";
+            this.btntitle.UseVisualStyleBackColor = true;
+            this.btntitle.Click += new System.EventHandler(this.btntitle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 301);
+            this.Controls.Add(this.btntitle);
+            this.Controls.Add(this.lbltitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitle;
+        private System.Windows.Forms.Button btntitle;
+    }
+}
+
